4e5024d075762a9c106908a567860787f6b576c4
mavlyut <334918@niuitmo.ru>
2022-05-30 22:50:03 +0300

    42

--- perf/bench_logger.cpp
+++ perf/bench_logger.cpp
@@ -28,12 +28,12 @@ public:
     }
   }
 
+
   void Write(const std::string& msg) const {
     size_t i = 0, msg_size = msg.size();
-    for (; i + BUFSIZE < msg.size(); i += BUFSIZE) {
+    for (; i + BUFSIZE < msg.size(); i += BUFSIZE, msg_size -= BUFSIZE) {
       strcpy(buf, msg.data() + i);
       write_buf(buf);
-      msg_size -= BUFSIZE;
     }
     strcpy(buf, msg.data() + i);
     write_buf(buf, msg_size);
